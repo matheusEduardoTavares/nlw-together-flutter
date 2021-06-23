e58f9a97d1bbf6fa6c1c831ec7a1c47d0cf27cc2
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-06-22 22:10:41 -0300

    Aula 02 - Maximum Speed - [WIP]: Adicionado classes abstratas para padronização de recursos (imagens, cores e estilos de texto)

---
+++ payflow/lib/shared/themes/app_images.dart
@@ -0,0 +1,14 @@
+///OBS: Uma classe que apenas contém 
+///atributos / métodos estáticos (no
+///caso só atributos), deve ser 
+///abstrata, afinal, não faz
+///sentido instanciá-la
+///Referência:
+///https://github.com/rocketseat-education/nlw-06-flutter/tree/images
+abstract class AppImages {
+  static const logoFull = "assets/images/logofull.png";
+  static const logomini = "assets/images/logomini.png";
+  static const union = "assets/images/union.png";
+  static const person = "assets/images/person.png";
+  static const google = "assets/images/google.png";
+}
