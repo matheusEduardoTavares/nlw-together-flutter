d333a9813cfadcc370eab19b3c7aeaa246209de2
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-06-24 00:25:14 -0300

    Aula 03 - In Orbit - [WIP]: Criado feature para fazer o login social

--- payflow/lib/modules/login/login_page.dart
+++ payflow/lib/modules/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -17,6 +18,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
+    final controller = LoginController();
 
     // return Scaffold(
     //   backgroundColor: AppColors.background,
@@ -85,9 +88,7 @@ class _LoginPageState extends State<LoginPage> {
                       top: 40.0
                     ),
                     child: SocialLoginButton(
-                      onTap: () {
-                        print('Clicou');
-                      },
+                      onTap: controller.googleSignIn,
                     ),
                   ),
                 ],
