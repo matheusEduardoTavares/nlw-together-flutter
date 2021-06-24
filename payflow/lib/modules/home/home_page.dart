a7859615ddaf605a75e46a4fe25a47b97edb6ba4
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-06-24 02:25:08 -0300

    Aula 03 - In Orbit - Adicionado o bottomNavigationBar na HomePage. Aula finalizada.

--- payflow/lib/modules/home/home_page.dart
+++ payflow/lib/modules/home/home_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final homeController = HomeController();
+  final _pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(152.0),
@@ -41,6 +58,54 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
+        ),
+      ),
+      body: _pages[homeController.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  homeController.setPage(0);
+                });
+              },
+              icon: Icon(
+                Icons.home,
+                color: AppColors.primary,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('Clicou');
+              },
+              child: Container(
+                height: 56,
+                width: 56,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5.0)
+                ),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  homeController.setPage(1);
+                });
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+            ),
+          ],
         ),
       ),
     );
